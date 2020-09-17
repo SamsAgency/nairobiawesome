4cfd508f127718b84c61b96b0a32b9e3c7961178
SamsAgency <samstore637@gmail.com>
2020-09-17 20:37:23 +0300

    404 page

--- src/Components/404/404.js
+++ src/Components/404/404.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "./404.css"
 
 const NothingHere = () => {
     return (
-        <div>
-            <h1>404</h1><br/>
-            <h3>Page not found</h3>
+        <div className="not-found">
+            <h1 className="big-font">404</h1>
+            <h3 className="subtitle-font">Something is wrong</h3>
         </div>
     )
 }
